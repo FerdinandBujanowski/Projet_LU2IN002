728abb37e5ab84e6ba8d0ed377a4f94f5535608a
FerdinandBujanowski <49587549+FerdinandBujanowski@users.noreply.github.com>
2022-12-04 19:24:42 +0100

    added other ant mages

--- src/GathererAnt.java
+++ src/GathererAnt.java
@@ -12,6 +12,12 @@ public class GathererAnt extends Ant {
         super.tick(terrain, barriers, predators, colonyData);
         if(this.predatorInProximity) {
             // run away from predator
+            assert this.closestPredatorPosition != null;
+            Point vectorToPredator = new Point(this.closestPredatorPosition.x - this.getX(), this.closestPredatorPosition.y - this.getY());
+            //invert vector pointing to predator
+            Point vectorFromPredator = new Point(vectorToPredator.x * -1, vectorToPredator.y * -1);
+            this.tryMoving(vectorFromPredator, barriers, predators, colonyData);
+
         } else {
             if(this.getLastInventoryIndex() == this.inventory.length - 1) {
                 //bring food to queen
@@ -20,7 +26,7 @@ public class GathererAnt extends Ant {
                     colonyData.onFeedQueenRequest(this.id);
                 } else {
                     Point vectorToQueen = new Point(queenPosition.x - this.getX(), queenPosition.y - this.getY());
-                    boolean movingSucceeded = this.tryMoveAlongVector(vectorToQueen, barriers, predators, colonyData);
+                    this.tryMoving(vectorToQueen, barriers, predators, colonyData);
                 }
             } else {
                 //gather more food
@@ -30,18 +36,7 @@ public class GathererAnt extends Ant {
                         this.inventory[this.getLastInventoryIndex() + 1] = terrain.videCase(closest.x, closest.y);
                     } else {
                         Point vectorToRessource = new Point(closest.x - this.getX(), closest.y - this.getY());
-                        if(this.tryMoveAlongVector(vectorToRessource, barriers, predators, colonyData)) {
-                            //moving succeeded
-                            this.calculateMovingCosts();
-                        } else {
-                            //if movement blocked, move other direction
-                            Point freePoint = this.getFreePoint(this.getPosition(), barriers, predators, colonyData);
-                            if(freePoint != null) {
-                                Point newVector = new Point(freePoint.x - this.getX(), freePoint.y - this.getY());
-                                this.tryMoveAlongVector(newVector, barriers, predators, colonyData);
-                                this.calculateMovingCosts();
-                            }
-                        }
+                        this.tryMoving(vectorToRessource, barriers, predators, colonyData);
                     }
                 }
             }
@@ -51,6 +46,22 @@ public class GathererAnt extends Ant {
                     this.currentEnergy += this.inventory[this.getLastInventoryIndex()].getQuantite();
                     this.inventory[this.getLastInventoryIndex()] = null;
                 }
+            }
+        }
+    }
+
+    //TODO put tryMoving() and calculateMovingCosts() into Animal class
+    private void tryMoving(Point vector, ArrayList<Barrier> barriers, ArrayList<Predator> predators, ColonyData colonyData) {
+        if(this.tryMoveAlongVector(vector, barriers, predators, colonyData)) {
+            //moving succeeded
+            this.calculateMovingCosts();
+        } else {
+            //if movement blocked, move other direction
+            Point freePoint = this.getFreePoint(this.getPosition(), barriers, predators, colonyData);
+            if(freePoint != null) {
+                Point newVector = new Point(freePoint.x - this.getX(), freePoint.y - this.getY());
+                this.tryMoveAlongVector(newVector, barriers, predators, colonyData);
+                this.calculateMovingCosts();
             }
         }
     }
